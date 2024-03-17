dc0854044eb8242996988b3bf33e0f9ceba29508
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2024-03-17 10:30:19 +0800

    initial app

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, useCameraPermissions } from "expo-camera/next";
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [facing, setFacing] = useState("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const [error, setError] = useState(null);
+  const [isCameraVisible, setIsCameraVisible] = useState(false);
+  const [barcode, setBarcode] = useState(null);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      try {
+        if (!permission?.granted) {
+          await requestPermission();
+        }
+      } catch (error) {
+        setError(error);
+      }
+    };
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    console.log(Math.random());
+    setBarcode(data);
+    setIsCameraVisible(false);
+  };
+
+  if (!permission) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>
+    );
+  }
+
+  if (!permission?.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>No access to camera</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>{error.toString()}</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isCameraVisible && (
+        <CameraView
+          style={styles.camera}
+          facing={facing}
+          onBarcodeScanned={handleBarCodeScanned}
+        />
+      )}
+      <View style={styles.barcodeContainer}>
+        {barcode && <Text>{barcode}</Text>}
+        <Button title="Scan" onPress={() => setIsCameraVisible(true)} />
+      </View>
     </View>
   );
 }
@@ -13,8 +72,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  camera: {
+    height: "100%",
+    width: "100%",
+  },
+  barcodeContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: "1rem",
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+    backgroundColor: "red",
+  },
+  button: {
+    backgroundColor: "green",
+    padding: "1rem",
   },
 });
